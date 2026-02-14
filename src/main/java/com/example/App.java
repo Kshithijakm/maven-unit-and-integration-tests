a01c2e261856404bed75d9a5252b270416cbf80d
Kshithijakm <kshithijakm024@gmail.com>
2026-02-14 12:18:44 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,17 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private MessageService service;
+
+    public App(MessageService service) {
+        this.service = service;
+    }
+
+    public String getFullMessage() {
+        return service.getGreeting();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("App is running!");
     }
 }
